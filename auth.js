6f333cd9b9f7f69a018957526dcae209992f376f
VinPixGame <yourvinzelf@gmail.com>
2026-08-22 18:35:27 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -305,3 +305,13 @@ if (!isRunningAsApp) {
     });
   }
 }
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then((reg) => console.log('Service Worker registered successfully!', reg.scope))
+      .catch((err) => console.log('Service Worker registration failed:', err));
+  });
+}
+
