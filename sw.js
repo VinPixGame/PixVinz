4485d6bb366c906043fdb3551f739e4a318d6dd1
VinPixGame <yourvinzelf@gmail.com>
2026-08-22 19:49:31 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker installed');
-});
-
-self.addEventListener('fetch', (e) => {
-  // Required for PWA criteria
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
